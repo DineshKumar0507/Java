f7b53874a571c8ac10f2c9c77daa99f0b34023b1
DineshKumar0507 <85884375+DineshKumar0507@users.noreply.github.com>
2021-06-20 23:18:26 +0530

    thread

---
+++ Example1.java
@@ -0,0 +1,29 @@
+class A extends Thread
+{
+ public void run()
+  {
+    for(int i=1;i<=10;i++)
+     {
+      System.out.println("In A:"+i);
+     }
+  }
+}
+class B extends Thread
+{
+ public void run()
+  {
+    for(int i=1;i<=10;i++)
+     {
+      System.out.println("In B:"+i);
+     }
+  }
+}
+public class Example1
+{
+  public static void main(String []args)
+ { A a=new A();
+  B b=new B();
+  a.start();
+  b.start();
+}
+}
