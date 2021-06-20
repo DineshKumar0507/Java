7d09cfd45e6145f97474a6fa32fe9f0541896699
DineshKumar0507 <85884375+DineshKumar0507@users.noreply.github.com>
2021-06-20 21:10:50 +0530

    using true false

---
+++ Declare.java
@@ -0,0 +1,11 @@
+public class Declare
+{
+public static void main(String []args)
+{
+boolean a= true;
+boolean b= false;
+System.out.println(!a);
+System.out.println(a|b);
+System.out.println((!a&b)|(a&!b));
+}
+}
